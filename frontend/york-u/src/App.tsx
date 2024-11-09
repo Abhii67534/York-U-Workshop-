a83a5691bf031de03d5783a73f5aad4b9259c930
Abhishek Sharma <abhisharma4950@gmail.com>
2024-11-08 20:11:30 -0500

    Removed meteors

--- frontend/york-u/src/App.tsx
+++ frontend/york-u/src/App.tsx
@@ -6,7 +6,7 @@ import { Button } from "./components/ui/button";
 function App() {
   return (
     <div className="bg-darkBg h-screen">
-      <Meteors />
+      
 
       {/* Header Section */}
       <header className="bg-gradient-to-r from-indigo-500 via-purple-600 to-pink-500 text-white text-center py-12 sm:py-16 lg:py-20 h-screen flex items-center justify-center">
